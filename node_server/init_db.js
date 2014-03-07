0411dc38325b652c1cc4e0db567d8bcb272e871d
Luis Zaman <zaman@13-182-102.client.wireless.msu.edu>
2014-03-07 10:12:09 -0500

    added very bad code to reset database

---
+++ node_server/init_db.js
@@ -0,0 +1,21 @@
+var r = require('rethinkdb');
+
+var connect = require('reql-then')
+  , reql = connect({
+      host: 'localhost',
+      port: 28015,
+      db: 'test',
+      authKey: '',
+      maxPoolSize: 10  // set to 1 to disable connection pooling
+    });
+
+
+var remove_old = r.dbDrop('opencfu_plates');
+var create_db = r.dbCreate('opencfu_plates');
+var create_table = r.db('opencfu_plates').tableCreate('plates');
+
+
+//Do this better!
+reql(remove_old);
+reql(create_db);
+reql(create_table);
