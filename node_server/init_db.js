756384ec8f21a9b27928244d1c0936d2f84bca3f
Luis Zaman <luis.zaman@gmail.com>
2014-11-05 21:55:34 -0800

    Update init_db.js

--- node_server/init_db.js
+++ node_server/init_db.js
@@ -1,21 +1,18 @@
 var r = require('rethinkdb');
-
-var connect = require('reql-then')
-  , reql = connect({
-      host: 'localhost',
-      port: 28015,
-      db: 'test',
-      authKey: '',
-      maxPoolSize: 10  // set to 1 to disable connection pooling
-    });
-
-
 var remove_old = r.dbDrop('opencfu_plates');
 var create_db = r.dbCreate('opencfu_plates');
 var create_table = r.db('opencfu_plates').tableCreate('plates', {primaryKey: "token"});
 
 
-//Do this better!
-reql(remove_old);
-reql(create_db);
-reql(create_table);
+var connection = null;
+r.connect({}, function(err, conn) {
+    if (err) throw err;
+
+	//Do this better!
+	remove_old.run(conn);
+	create_db.run(conn);
+	create_table.run(conn);
+
+});
+
+
